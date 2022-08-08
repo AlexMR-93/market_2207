dfa509499edd9c1438846b4b1fb5812b29aede42
Alex M <Alexm717@hotmail.com>
2022-08-08 08:46:18 -0700

    test inventory list

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -22,5 +22,17 @@ describe(Vendor) do
     expect(@vendor.check_stock(@item1)).to(eq(0))
     @vendor.stock(@item1, 30)
     expect(@vendor.inventory).to(eq({@item1 => 30}))
+    @vendor.check_stock(@item1)
+    expect(@vendor.check_stock(@item1)).to(eq(30))
+    @vendor.stock(@item1, 25)
+    @vendor.check_stock(@item1)
+    expect(@vendor.check_stock(@item1)).to(eq(55))
+  end
+
+  it("can see the inventory list") do
+    @vendor.stock(@item1, 30)
+    @vendor.stock(@item1, 25)
+    @vendor.stock(@item2, 12)
+    expect(@vendor.inventory).to(eq(    @item1 => 55,     @item2 => 12))
   end
 end
